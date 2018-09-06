9b332cbfe3dcf2a7f4e7cca4b8f289598b446c72
Jeslienss <lijwnss@gmail.com>
2018-09-05 22:29:38 -0400

    part sbox

--- des.h
+++ des.h
@@ -10,9 +10,10 @@
 #include <stdint.h>
 
 void convertStr2Hex(char* key, uint64_t* binKey);
-void genPermutedKey(uint64_t* binKey, uint64_t *subKey);
+void genSubKey(uint64_t* binKey, uint64_t *subKey);
 void des(char* key);
 uint64_t ariShift(uint64_t f, uint8_t t);
+void encodeData(uint64_t *subKey, uint64_t *m);
 
 
 #endif //DES_DES_H
